e047f4f839b30137bc031201ad9b6b4299b600b8
NicoSorren <nsorrentino2001@gmail.com>
2025-02-20 11:28:30 -0300

    commit

--- product_manager.py
+++ product_manager.py
@@ -89,6 +89,7 @@ class ProductManager:
 
         # 1) Seleccionamos la categoría FUERA del formulario
         categorias = self.df["CATEGORIA"].astype(str).str.strip().unique().tolist()
+        categorias.sort()
         categoria_seleccionada = st.selectbox("Selecciona la categoría", options=categorias, key="delete_category_selectbox")
 
         # Filtramos los productos de esa categoría
